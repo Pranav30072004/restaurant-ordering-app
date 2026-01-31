5376b9ffb05c532b5e81e74d7c172272c72d7751
Pranav30072004 <pranavrpathak04@gmail.com>
2026-01-31 18:16:53 +0530

    created JS based cart render
    Moved from hardcoded variant of restaurant cart to a JS, state based cart that is dependent on an array of selected food items, which is used to render the cart html template

--- Ordering.js
+++ Ordering.js
@@ -1,17 +1,59 @@
 import {menuArray} from "./data.js";
 
 const menuList = document.getElementById("main-menu");
+const cartDisplay = document.getElementById('item-container');
+const totalCartPrice = document.getElementById('total-price');
 
 document.addEventListener('click', (e) => {
-    if(e.target.dataset.add) {
-        console.log(e.target.dataset.add);
+    if (e.target.dataset.add) {
+        document.getElementById('cart').style.display = 'flex';
+        handleAddClick(e.target.dataset.add);
     }
 })
+let foodObjects = [];
+
+function handleAddClick(foodId) {
+    foodObjects.push(menuArray.find( (foodObject) => {
+      return foodObject.id === Number(foodId);
+    }) )
+
+    cartDisplay.innerHTML = renderCart();
+
+}
+
+function renderCart() {
+    let cartHTML = ``
+    foodObjects.forEach( (foodObject) => {
+        cartHTML += `
+        <div class="cart-item">
+            <div class="cart-text">
+                <p>${foodObject.name}</p>
+                <p role="button" 
+                class="remove-btn" 
+                data-remove=${foodObject.id}>remove</p>
+            </div>
+            <p class="cart-price">$${foodObject.price}</p>
+        </div>
+        `
+    })
+    totalCartPrice.innerText = `$${getTotalPrice()}`;
+    return cartHTML
+
+
+}
+
+function getTotalPrice() {
+    const totalPrice = foodObjects.reduce((total, itemPrice) => {
+        return total + itemPrice.price;
+    }, 0)
+    return totalPrice;
+}
+
 
 const menuHTML = menuArray.map( (item) => {
     return `<div class="menu-item">
         <span class="food-icon"
-        rol="img"
+        role="img"
         aria-hidden="true">
         ${item.emoji}
         </span>
@@ -23,7 +65,7 @@ const menuHTML = menuArray.map( (item) => {
         </div>
         <button class="add-item-btn" 
         aria-label="add menu item to cart"
-        data-add="${item.id}">+</button>
+        data-add=${item.id}>+</button>
     </div>`
 }).join('');
 
